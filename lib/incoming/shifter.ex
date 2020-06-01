c24d78622e88d5aad07461fd4ccaf11e7e41041e
otherchris <chriscaragianis@gmail.com>
2020-06-01 15:56:01 -0400

    i think it works

--- lib/incoming/shifter.ex
+++ lib/incoming/shifter.ex
@@ -4,6 +4,7 @@ defmodule Incoming.Shifter do
   use GenServer
 
   alias Incoming.Shift
+  alias Incoming.User
 
   # Client API
 
@@ -20,39 +21,44 @@ defmodule Incoming.Shifter do
 
   @impl true
   def init(:ok) do
-    Process.send(self(), :tick, [])
+    Process.send(self(), :shift, [])
     {:ok, %{}}
-  end
-
-  def handle_info(:tick, state) do
-    IO.inspect "tick"
-    Process.send_after(self(), :tick, 5000, [])
-    {:noreply, state}
   end
 
   def handle_info(:shift, state) do
     {start, delay} = time_and_delay()
     {:ok, shifts} = Shift.get_by_start(start)
-    phones 
-    |> Enum.map(&Map.get(&1, :user_id))
-    |> Enum.map(&User.get_user(&1))
-    |> Enum.map(%Map.get(&1, :phone)) 
-    Process.send_after(self(), {:numbers, phones}, delay, [])
+    phones = 
+      shifts
+      |> Enum.map(&Map.get(&1, :user_id))
+      |> Enum.map(&User.get_user(&1))
+      |> Enum.map(&Map.get(&1, :phone)) 
+    Process.send_after(self(), {:numbers, phones}, 5, [])
     Process.send_after(self(), :shift, 30 * 60 * 60 * 1000)
+    {:noreply, state}
+  end
+
+  def handle_info({:numbers, phones}, state) do
+    IO.inspect "applying new numbers at #{DateTime.utc_now() |> DateTime.to_string()}"
+    pid = Process.whereis(:dialer)
+    IncomingDialer.set_incoming_numbers(pid, phones)
     {:noreply, state}
   end
 
   defp time_and_delay do
     now = DateTime.utc_now
     next = DateTime.utc_now
-    if now.minute > 30 do
-      next.minute = 0
-      next.hour = DateTime.add(now, 60 * 60, :second).hour 
-    else
-      next.minute = 30
-    end
+    next = 
+      if now.minute > 30 do
+        next
+        |> Map.put(:second, 0)
+        |> Map.put(:minute, 0)
+        |> Map.put(:hour, DateTime.add(now, 60 * 60, :second).hour)
+      else
+        Map.put(next, :minute, 30)
+      end
     next = DateTime.truncate(next, :second)
-    delta = DateTime.diif(next, now, :millisecond)
+    delta = DateTime.diff(next, now, :millisecond)
     {next, delta}
   end
 end
