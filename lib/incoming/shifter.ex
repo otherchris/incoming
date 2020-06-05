95778cb89f6f622dfdd3b95e41aac0cdbfa3bfaa
otherchris <chriscaragianis@gmail.com>
2020-06-05 15:26:11 -0400

    tighten timeframe, add logging

--- lib/incoming/shifter.ex
+++ lib/incoming/shifter.ex
@@ -2,6 +2,7 @@ defmodule Incoming.Shifter do
   @moduledoc false
 
   use GenServer
+  require Logger
 
   alias Incoming.Shift
 
@@ -19,17 +20,26 @@ defmodule Incoming.Shifter do
 
   @impl true
   def init(:ok) do
-    Process.send(self(), :shift, [])
+    Process.send(self(), :tick, [])
     {:ok, %{}}
   end
 
   @impl true
-  def handle_info(:shift, state) do
-    now =
+  def handle_info(:tick, state) do
+    next =
       DateTime.utc_now()
       |> DateTime.truncate(:second)
       |> Map.put(:second, 0)
+    if DateTime.diff(DateTime.utc_now, next) > 50 do
+      Process.send(self(), {:shift, DateTime.add(next, 60, :second)}, [])
+    end
+    Process.send_after(self(), :tick, 6000, [])
+    {:noreply, state}
+  end
 
+  @impl true
+  def handle_info({:shift, now}, state) do
+    Logger.info("Processing shifts for #{now} at #{DateTime.utc_now()}")
     {:ok, start_shifts} = Shift.get_by_start(now)
     {:ok, stop_shifts} = Shift.get_by_stop(now)
 
@@ -49,7 +59,7 @@ defmodule Incoming.Shifter do
     stop_phones
     |> Enum.each(&IncomingDialer.remove_incoming_number(d, &1))
 
-    Process.send_after(self(), :shift, 60_000, [])
+    Logger.info("Phones added: #{start_phones}, Phones removed: #{stop_phones}")
 
     {:noreply, state}
   end
